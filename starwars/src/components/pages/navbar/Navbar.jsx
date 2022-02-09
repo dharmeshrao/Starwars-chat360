cbe9d3873c35f7c5ba886a118e10ef108b172893
Dharmesh <yadavdydv@gmail.com>
2022-02-09 14:05:34 +0530

    created navbar

---
+++ starwars/src/components/pages/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+export const Navbar = ()=>{
+    return <nav>
+        <ul className="flex bg-purple-600 h-12 items-center justify-end text-white cursor-pointer ">
+            <li className="mx-7">Home</li>
+            <li className="mx-7">About Us</li>
+            <li className="mx-7">Contact</li>
+            <li className="mx-7">Characters</li>
+        </ul>
+    </nav>
+}
