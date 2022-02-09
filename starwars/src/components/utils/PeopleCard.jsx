914c3b5dc655d44cd0ead7231f1f42c8a8efea48
Dharmesh <yadavdydv@gmail.com>
2022-02-09 19:33:40 +0530

    peopleCard complete

--- starwars/src/components/utils/PeopleCard.jsx
+++ starwars/src/components/utils/PeopleCard.jsx
@@ -1,4 +1,5 @@
-import { PeopleData } from "./Axios";
+import { useState, useEffect, useCallback } from "react";
+
 export const PeopleCard = ({
   name,
   birth,
@@ -7,6 +8,22 @@ export const PeopleCard = ({
   starships,
   vehicles,
 }) => {
+  const [peopleFilm, setPeopleFilm] = useState([]);
+  const [peopleVehicles,setPeopleVehicles] = useState([]);
+  const [peopleStarships,setPeopleStarships] = useState([]);
+  const fetchAll = async (data,func) => {
+    const results = await Promise.all(
+      data.map((url) => fetch(url).then((r) => r.json()))
+    );
+    func(results);
+  };
+
+  useEffect(() => {
+    fetchAll(films,setPeopleFilm);
+    fetchAll(starships,setPeopleStarships);
+    fetchAll(vehicles,setPeopleVehicles)
+  }, [films,starships,vehicles]);
+
   return (
     <div className="w-4/5 p-4 bg-white m-auto rounded-xl drop-shadow-lg flex flex-col gap-2">
       <div className="flex justify-around text-center p-4 bg-purple-100 bg-opacity-40 rounded-xl">
@@ -23,36 +40,42 @@ export const PeopleCard = ({
           <p className="text-black text-2xl font-bold">{gender || ""}</p>
         </div>
       </div>
-      <div className="bg-purple-100 bg-opacity-30 rounded-xl h-8 flex p-6 items-center">
+      <div className="bg-purple-100 bg-opacity-30 rounded-xl flex flex-wrap p-4 gap-y-4 items-center">
         <h2 className="font-semibold text-xl mr-9 text-black">Films: </h2>
-        {films &&
-          films?.length > 0 &&
-          films.map((e, i) => (
+        {peopleFilm &&
+          peopleFilm?.length > 0 &&
+          peopleFilm.map((e, i) => (
             <button
               key={i}
               className="bg-white ml-4 hover:bg-gray-100 text-purple-600 font-semibold py-1 px-4 border border-purple-600 rounded shadow"
             >
-              {i + 1}
+              {e.title}
             </button>
           ))}
       </div>
-      <div className="bg-purple-100 bg-opacity-40 rounded-xl h-8 flex p-6 items-center">
+      <div className="bg-purple-100 bg-opacity-30 rounded-xl flex flex-wrap p-4 gap-y-4 items-center">
         <h2 className="font-semibold text-xl text-black">Starships: </h2>
-        {starships &&
-          starships?.length > 0 &&
-          starships.map((e, i) => (
-            <button key={i} className="bg-white ml-4 hover:bg-gray-100 text-purple-600 font-semibold py-1 px-4 border border-purple-600 rounded shadow">
-              {i + 1}
+        {peopleStarships &&
+          peopleStarships?.length > 0 &&
+          peopleStarships.map((e, i) => (
+            <button
+              key={i}
+              className="bg-white ml-4 hover:bg-gray-100 text-purple-600 font-semibold py-1 px-4 border border-purple-600 rounded shadow"
+            >
+              {e.name}
             </button>
           ))}
       </div>
-      <div className="bg-purple-100 bg-opacity-40 rounded-xl h-8 flex p-6 items-center">
+      <div className="bg-purple-100 bg-opacity-30 rounded-xl flex flex-wrap p-4 gap-y-4 items-center">
         <h2 className="font-semibold text-xl mr-2 text-black">Vehicles: </h2>
-        {vehicles &&
-          vehicles.length > 0 &&
-          vehicles.map((e, i) => (
-            <button key={i} className="bg-white ml-4 hover:bg-gray-100 text-purple-600 font-semibold py-1 px-4 border border-purple-600 rounded shadow">
-              {i+1}
+        {peopleVehicles &&
+          peopleVehicles.length > 0 &&
+          peopleVehicles.map((e, i) => (
+            <button
+              key={i}
+              className="bg-white ml-4 hover:bg-gray-100 text-purple-600 font-semibold py-1 px-4 border border-purple-600 rounded shadow"
+            >
+              {e.name}
             </button>
           ))}
       </div>
