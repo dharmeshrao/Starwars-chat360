b48cb4a08282ee8d3d7fd9497adea7f88ee1e6e5
Dharmesh <yadavdydv@gmail.com>
2022-02-10 11:31:53 +0530

    started working on starships page

--- starwars/src/components/Routes.jsx
+++ starwars/src/components/Routes.jsx
@@ -1,14 +1,16 @@
 import { Routes as Main, Route } from "react-router-dom";
 import { Films } from "./pages/films/Films";
-import { Navbar} from "./pages/navbar/Navbar"
+import { Navbar } from "./pages/navbar/Navbar";
 import { PeoplePage } from "./pages/people/PeoplePage";
+import { Starships } from "./pages/starships/Starships";
 export const Routes = () => {
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <Main>
-        <Route path="/" element={<PeoplePage/>} />
-        <Route path="/films/:id" element={<Films/>}/>
+        <Route path="/" element={<PeoplePage />} />
+        <Route path="/films/:id" element={<Films />} />
+        <Route path="/starships/:id" element={<Starships />} />
         <Route path="*" element={<h1>No page found...</h1>} />
       </Main>
     </>
